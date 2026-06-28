79706d03abe6ed74a54830c7080f90a82fa48583
Thiago Moraes <81823495+thivgox@users.noreply.github.com>
2026-06-28 16:40:27 -0300

    Add files via upload

---
+++ src/main/java/com/example/examatch/ExamatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.examatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamatchApplication.class, args);
+	}
+
+}
